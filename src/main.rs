56c72daacb4735a45a40544debb57bb149e05a53
Sebastián Romero Cruz <sebastianromerocruz@gmail.com>
2022-04-09 20:21:38 -0400

    Can now accept commands (such as ls and pwd), arguments (such as -a), and change directories using cd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+
+const CD_COMMAND: &str = "cd";
+
+fn main(){
+    loop {
+        // Prepare prompt by:
+        //     1. Using the `>` character as the prompt
+        //     2. Flushes standard output to ensure `>` prints before `read_line()`
+        print!("> ");
+        io::stdout().flush().expect("FLUSH: Thread panicked!");
+
+        // Read user input into an empty string
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("ERROR: Failed to read command.");
+
+        // Isolates command and arguments by:
+        //     1. Trimming and splitting the input by the whitespace (`" "`)
+        //     2. Grabs the zero-th element from the `SplitWhitespace` object (`next()`) as the
+        //        command
+        //     3. Leaves the rest as the arguments
+        let mut argument_vector = user_input.trim().split_whitespace();
+        let command = argument_vector.next().unwrap();
+        let arguments = argument_vector;
+
+        match command {
+            CD_COMMAND => {
+                // Default to '/' as new directory if one was not provided
+                let new_directory = arguments
+                    .peekable()
+                    .peek()
+                    .map_or("/", |x| *x);
+                let root = Path::new(new_directory);
+
+                if let Err(e) = std::env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            command => {
+                // Initiates a new child process:
+                //     1. Using `command` as the command...
+                //     2. ...and `arguments` as the arguments
+                let mut child_process = Command::new(command)
+                    .args(arguments)
+                    .spawn()
+                    .unwrap();
+
+                // Waits so as to not accept another command until this one completes
+                child_process.wait().expect("WAIT: Thread panicked");
+            }
+        }
+    }
 }
